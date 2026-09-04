13e13bfaf9b67b158184dd1b4bffd76bee80b393
Christian <chrwolsing@mail.com>
2026-09-05 00:01:02 +0200

    VS Code extensions from unstable

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
 
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
